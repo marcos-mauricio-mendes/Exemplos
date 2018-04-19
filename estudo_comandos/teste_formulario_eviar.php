c0bc9169fc32988de3ca79ee9c032a40418bccc3
Marcos Mauricio Mendes Alves <mauricio3ma@gmail.com>
2018-04-19 07:54:47 -0300

    Comandos HTML e Diversos
    Comandos HTML e Diversos

---
+++ estudo_comandos/teste_formulario_eviar.php
@@ -0,0 +1,44 @@
+﻿<html>
+ 
+   <head>
+      <title>Index</title>
+      <style type="text/css">
+         div {
+            width: 400px;
+            margin: 30px auto;
+            border: 1px solid #999;
+            padding: 25px;
+            border-radius: 7px;
+            box-shadow: 2px 2px 3px 1px #999;
+         }
+      </style>
+   </head>
+ 
+   <?php
+   function teste() {
+   ?>
+      <div>
+         <p>
+            <?php echo "Foi digitado \"" . $_POST['texto'] . "\""; ?>
+         </p>
+      </div>
+   <?php   
+   }
+   ?>   
+ 
+   <body>
+      <div>
+         <form action="" method="POST">
+            <input type="text" name="texto" />
+            <br />   
+            <input type="submit" value="Enviar..." />
+         </form>
+      </div>
+      <?php
+      if ( isset( $_GET['a'] ) && $_GET['a'] == 'ok' && $_POST['texto'] != '' ) {
+         teste();
+      }
+      ?>
+   </body>
+ 
+</html>
